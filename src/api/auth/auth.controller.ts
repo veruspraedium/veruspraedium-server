4bb351bbc07f8ed32c5367e7ff66be14fdaaedbf
titanv <a01098930999@gmail.com>
2021-01-21 23:09:22 +0900

    update - use dotenv

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -1,4 +1,16 @@
+import MongoClient from 'mongodb';
+MongoClient.MongoClient;
+var url = process.env.url;
+var db;
 
+MongoClient.connect(url, function (err, database) {
+   if (err) {
+      console.error('MongoDB 연결 실패', err);
+      return;
+   }
+
+   db = database;
+});
 
 export const login = (async (ctx,next) => {
   console.log("asdasdasdad");
