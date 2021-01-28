e7cf976666cbb909b991e9e0cb89e8cc63f1383e
TitanV <a01098930999@gmail.com>
2021-01-28 15:42:38 +0900

    update - more strictfull model

--- src/model/user.ts
+++ src/model/user.ts
@@ -3,15 +3,14 @@ const Schema = mongoose.Schema;
 
 
 const userSchema = new Schema({
-  id: String,
+  id: {type: String, unique: true},
   password: String,
-  nickname: String,
+  nickname: {type: String,unique: true},
   address: String,
   introduce: { type: String, default: "default introduce" },
   profileImage: { type: String, default: "no image" },
   isWriter: { type: Boolean, default: false },
   subscribe: { type: Array, default: [] },
-  lastLogin: { type: Date, default: Date.now },
   cert: { type: Boolean, default: false },
   published_date: { type: Date, default: Date.now }
 });
