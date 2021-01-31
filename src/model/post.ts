e3255f92a9a0072bc652509eea03f7d196acefae
TitanV <a01098930999@gmail.com>
2021-01-31 20:19:19 +0900

    add - post model

---
+++ src/model/post.ts
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+
+const postSchema = new Schema({
+  id: {type: String, unique: true},
+  password: String,
+  nickname: {type: String,unique: true},
+  address: String,
+  introduce: { type: String, default: "default introduce" },
+  profileImage: { type: String, default: "no image" },
+  isWriter: { type: Boolean, default: false },
+  subscribe: { type: Array, default: [] },
+  cert: { type: Boolean, default: false },
+  published_date: { type: Date, default: Date.now }
+});
+
+
+export default mongoose.model('post', postSchema);
