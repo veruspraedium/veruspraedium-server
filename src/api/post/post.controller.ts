87878e3cdffbed3973314e55864db3904e282729
TitanV <a01098930999@gmail.com>
2021-02-01 14:11:50 +0900

    add - post api controller

--- src/api/post/post.controller.ts
+++ src/api/post/post.controller.ts
@@ -1,6 +1,21 @@
+import mongoose from 'mongoose';
+import redis from 'async-redis';
+import crypto from 'crypto';
+import User from '../../model/user';
+import { jwtverify } from '../../lib/token';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const client = redis.createClient();
+const mongoConfig = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+};
+mongoose.connect(process.env.uri, mongoConfig);
 
 
-export const login = (async (ctx,next) => {
+export const post = (async (ctx) => {
   console.log("asdasdasdad");
   
 
