7cb0f377511e5ec4eb53719db479ce304c080b43
TitanV <a01098930999@gmail.com>
2021-02-01 15:07:49 +0900

    update - not remove file extension

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,9 +3,14 @@ import multer from '@koa/multer';
 import auth from './admin';
 
 const api = new Router();
-const upload = multer({ dest: './files/' });
+const storage = multer.diskStorage({
+  destination: async (req, file, cb) => { cb(null, './files/') },
+  filename: async (req, file, cb) => { cb(null,`${Date.now()}-${file.originalname}`) }
+});
+const upload = multer({ storage: storage });
 
-import {signUp, loadProfile, changeProfile, userSecession, duplicateCheck, verification, login, logout, requestAccessToken, findPassword} from './auth/auth.controller';
+
+import {signUp, loadProfile, changeProfile, userSecession, duplicateCheck, verification, login, logout, requestAccessToken, findPassword, loadImage} from './auth/auth.controller';
 import {post} from './post/post.controller';
 
 api.use('/admin', auth.routes());
@@ -20,6 +25,7 @@ api.post('/login', login);
 api.delete('/logout', logout);
 api.get('/token', requestAccessToken);
 api.post('/findpassword', findPassword);
+api.get('/loadimage/:imagepath', loadImage);
 
 api.post('/post', post);
 
