fe8ecafceb6b9d8375b376d0895496e6c0fde060
titanv <a01098930999@gmail.com>
2021-02-03 01:15:18 +0900

    update - indent

--- src/model/user.ts
+++ src/model/user.ts
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   id: {type: String, unique: true},
-  password: String,
-  nickname: {type: String,unique: true},
-  address: String,
+  password: { type: String },
+  nickname: {type: String, unique: true},
+  address: { type: String },
   introduce: { type: String, default: "default introduce" },
   profileImage: { type: String, default: "no image" },
   isWriter: { type: Boolean, default: false },
